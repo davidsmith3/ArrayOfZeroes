eca6091c9aaf065ab01eed02f5cd5fa16c54e03c
davidsmith3 <david.smith3@outlook.com>
2014-11-30 14:22:38 -0500

    initial commit

---
+++ Sorter.Test/UnitTest1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sorter.Test {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestEmpty() {
+            SorterCS.Sorter s = new SorterCS.Sorter();
+            int[] nums = new int[0];
+            int[] result = s.Sort(nums);
+
+            Assert.AreEqual(0, result.Length);
+        }
+
+        [TestMethod]
+        public void TestOneElementNonZero() {
+            SorterCS.Sorter s = new SorterCS.Sorter();
+            int[] nums = { 1 };
+            int[] result = s.Sort(nums);
+
+            Assert.AreEqual(1, result.Length);
+            Assert.AreEqual(1, result[0]);
+        }
+
+        [TestMethod]
+        public void TestOneElementZero() {
+            SorterCS.Sorter s = new SorterCS.Sorter();
+            int[] nums = { 0 };
+            int[] result = s.Sort(nums);
+
+            Assert.AreEqual(1, result.Length);
+            Assert.AreEqual(0, result[0]);
+        }
+
+        [TestMethod]
+        public void TestTwoElementsInOrder() {
+            SorterCS.Sorter s = new SorterCS.Sorter();
+            int[] nums = { 6, 0 };
+            int[] result = s.Sort(nums);
+
+            Assert.AreEqual(2, result.Length);
+            Assert.AreEqual(0, result[1]);
+        }
+
+        [TestMethod]
+        public void TestTwoElementsOutOfOrder() {
+            SorterCS.Sorter s = new SorterCS.Sorter();
+            int[] nums = { 0, 6 };
+            int[] result = s.Sort(nums);
+
+            Assert.AreEqual(2, result.Length);
+            Assert.AreEqual(0, result[1]);
+        }
+
+        [TestMethod]
+        public void TestTwoElementsBothZero() {
+            SorterCS.Sorter s = new SorterCS.Sorter();
+            int[] nums = { 0, 0 };
+            int[] result = s.Sort(nums);
+
+            Assert.AreEqual(2, result.Length);
+            Assert.AreEqual(0, result[1]);
+            Assert.AreEqual(0, result[0]);
+        }
+
+        [TestMethod]
+        public void TestTwoElementsBothNonZero() {
+            SorterCS.Sorter s = new SorterCS.Sorter();
+            int[] nums = { 2, 2 };
+            int[] result = s.Sort(nums);
+
+            Assert.AreEqual(2, result.Length);
+        }
+
+        [TestMethod]
+        public void TestTypical() {
+            SorterCS.Sorter s = new SorterCS.Sorter();
+            int[] nums = { 1, 0, 2, 0, 0, 3, 4 };
+            int[] result = s.Sort(nums);
+
+            Assert.AreEqual(7, result.Length);
+            Assert.AreEqual(0, result[6]);
+            Assert.AreEqual(0, result[5]);
+            Assert.AreEqual(0, result[4]);
+        }
+
+        [TestMethod]
+        public void TestTypicalLong() {
+            SorterCS.Sorter s = new SorterCS.Sorter();
+            int[] nums = { 1, 0, 2, 0, 0, 3, 4, 2, 4, 5, 0, 2, 3, 6, 0, 2, 3, 0 };
+            int[] result = s.Sort(nums);
+
+            Assert.AreEqual(18, result.Length);
+            Assert.AreEqual(0, result[17]);
+            Assert.AreEqual(0, result[16]);
+            Assert.AreEqual(0, result[15]);
+            Assert.AreEqual(0, result[14]);
+            Assert.AreEqual(0, result[13]);
+            Assert.AreEqual(0, result[12]);
+        }
+
+
+    }
+}
---
+++ SorterCS/Sorter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SorterCS
+{
+    public class Sorter
+    {
+        public int[] Sort(int[] nums) {
+            if ((nums.Length == 0) || (nums.Length == 1)) return nums;
+            int lo = 0;
+            int hi = nums.Length - 1;
+            do {
+                while ((hi > lo) && (nums[hi] == 0)) hi--;
+                while ((lo < hi) && (nums[lo] != 0)) lo++;
+                if (lo < hi) Swap(nums, lo, hi);
+            } while (lo < hi);
+            return nums;
+        }
+
+        private void Swap(int[] nums, int p, int q) {
+            int temp = nums[p];
+            nums[p] = nums[q];
+            nums[q] = temp;
+        }
+    }
+}
